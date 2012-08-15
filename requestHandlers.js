93eb9a1fe5b5e1a053969e7f08ac33332b9826ac
Sapardi <sapardi@ptaskes.com>
2012-08-15 10:05:43 +0700

    add upload fitur

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,23 +1,62 @@
-var exec = require("child_process").exec;
+//var exec = require("child_process").exec;
+var querystring = require("querystring"),
+	fs = require("fs"); 
 
-function start(res) {
+function start(res, postData) {
 	console.log("Request handler 'start' was called.");
-	var content = "empty";
+	//var content = "empty";
 	
-	exec("find /", { timeout: 10000, maxBuffer: 20000*1024 }, 
+	/*exec("find /", { timeout: 10000, maxBuffer: 20000*1024 }, 
 		function (error, stdout, stderr) {
 		res.writeHead(200, {"Content-Type": "text/plain"});
 		res.write(stdout);
 		res.end();	
-	});		
+	});*/
+	
+	// View and Controller logic in one module
+	var body = '<html>' +
+		'<head>'+
+		'<meta http-equiv="Content-Type" content="text/html; '+
+		'charset=UTF-8" />'+
+		'</head>'+
+		'<body>'+
+		'<form action="/upload" method="post">'+
+    	'<textarea name="text" rows="20" cols="60"></textarea>'+
+    	'<input type="submit" value="Submit text" />'+		
+		'</form>'+
+		'</body>'+
+		'</html>';
+	
+	res.writeHead(200, {"Content-Type": "text/html"});
+	res.write(body);
+	res.end();
+			
 }
 
-function upload(res) {
+function upload(res, postData) {
 	console.log("Request handler 'upload' was called.");
 	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.write("Hello Upload");
+	res.write("You've sent a text: " + querystring.parse(postData).text);
 	res.end();
 }
 
+function show(res, postData) {
+	console.log("Request handler 'show' was called.");
+	fs.readFile("/tmp/test.png", "binary", function(error, file) {
+		if(error) {
+			res.writeHead(500, {"Content-Type": "text/plain"});
+			res.write(error + "\n");
+			res.end();				
+		} else {
+			res.writeHead(200, {"Content-Type": "image/png"});
+			res.write(file, "binary");
+			res.end();		
+		}
+	})		
+}
+
+
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
--- server.js~
+++ server.js~
@@ -26,10 +26,23 @@ var url = require("url");
 
 function start(route, handle) {
 	function onRequest(req, res) {
+		var postData = "";
 		var pathname = url.parse(req.url).pathname;
 		console.log("Request for: " + pathname + " received.");
 		
 		route(handle, pathname, res);
+		
+		// Add Event Handling
+		req.setEncoding('utf8');
+		
+		req.addListener("data", function(postDataChunk) {
+			postData = postDataChunk;
+			console.log("Received POST data chunk " + postDataChunk +".");		
+		});
+		
+		req.addListener("end", function() {
+			route(handle, pathname, res, postData);				
+		});
 	}	
 	
 	http.createServer(onRequest).listen(8888);
